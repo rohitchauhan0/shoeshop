2cf199d5aa03c3060383687df36f4201302ba0c3
Devsthana <devsthana@gmail.com>
2024-03-20 18:53:23 +0530

    third commit

--- Server/index.js
+++ Server/index.js
@@ -32,7 +32,7 @@ app.use(cookieParser())
 const cors = require('cors')
 app.use(cors({
     credentials:true,
-    origin:"http://localhost:3000"
+    origin:"https://shoeshop-phi.vercel.app"
 }))
 
 //mounting
